ee155fdf1b3b5e6c2211f9e77ad458b5a40b0d31
NunooAfonsoo <nuno.miguel.leitao.afonso@gmail.com>
2023-03-16 23:01:48 +0000

    Added managers and MainMenu and SinglePlayer Scenes

---
+++ Assets/Scripts/Managers/GameManager.cs
@@ -0,0 +1,69 @@
+using System;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance;
+
+    public const int MAX_NUMBER_OF_CACTI = 5;
+    public int NumberOfActiveCacti { get; private set; }
+
+    private int cactiDestroyed;
+    public enum GameState
+    {
+        Starting,
+        OnGoing,
+        GameEnd
+    }
+    public GameState CurrentGameState { get; private set; }
+
+    public event EventHandler OnGameWon;
+    public event EventHandler OnGameCountdownEnded;
+    public event EventHandler<OnCactusDestroyedArgs> OnCactusDestroyed;
+    public class OnCactusDestroyedArgs : EventArgs
+    {
+        public int score;
+        public OnCactusDestroyedArgs(int score)
+        {
+            this.score = score;
+        }
+    }
+
+    private void Awake()
+    {
+        Instance = this;
+
+        CurrentGameState = GameState.Starting;
+        cactiDestroyed = 0;
+        NumberOfActiveCacti = 0;
+    }
+
+    internal bool CanSpawnCactus()
+    {
+        return NumberOfActiveCacti < MAX_NUMBER_OF_CACTI;
+    }
+
+    public void CactusSpawned()
+    {
+        NumberOfActiveCacti++;
+    }
+
+    public void CactusDestroyed()
+    {
+        cactiDestroyed ++;
+        OnCactusDestroyed.Invoke(this, new OnCactusDestroyedArgs(cactiDestroyed));
+        NumberOfActiveCacti--;
+
+        if (cactiDestroyed == 2)
+        {
+            CurrentGameState = GameState.GameEnd;
+            OnGameWon.Invoke(this, EventArgs.Empty);
+        }
+    }
+
+    public void StartCountdownEnded()
+    {
+        CurrentGameState = GameState.OnGoing;
+        OnGameCountdownEnded.Invoke(this, EventArgs.Empty);
+    }
+}
---
+++ Assets/Scripts/Managers/UIManager.cs
@@ -0,0 +1,79 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class UIManager : MonoBehaviour
+{
+    public static UIManager Instance;
+
+    [SerializeField] private GameObject startGamePanel;
+    [SerializeField] private TextMeshProUGUI getReadyTimeText;
+    [SerializeField] private TextMeshProUGUI scoreText;
+    [SerializeField] private TextMeshProUGUI timeText;
+    [SerializeField] private TextMeshProUGUI ammoText;
+    [SerializeField] private GameObject gameWonPanel;
+    [SerializeField] private TextMeshProUGUI gameWonMessage;
+
+    private GameManager gameManager;
+    private float elapsedTime;
+    private const int GET_READY_TIME = 0;
+
+    private void Awake()
+    {
+        Instance = this;
+
+        elapsedTime = 0;
+    }
+
+    private void Start()
+    {
+        gameManager = GameManager.Instance;
+        gameManager.OnCactusDestroyed += GameManager_OnCactusDestroyed;
+        gameManager.OnGameWon += GameManager_OnGameWon;
+    }
+
+    private void Update()
+    {
+        elapsedTime += Time.deltaTime;
+        switch (gameManager.CurrentGameState)
+        {
+            case GameManager.GameState.Starting:
+                getReadyTimeText.text = (GET_READY_TIME - elapsedTime).ToString("F0");
+                if(elapsedTime >= GET_READY_TIME)
+                {
+                    StartGame();
+                }
+                break;
+            case GameManager.GameState.OnGoing:
+                timeText.text = "Time: " + elapsedTime.ToString("F2");
+                break;
+        }        
+    }
+
+    private void StartGame()
+    {
+        getReadyTimeText.gameObject.SetActive(false);
+        startGamePanel.SetActive(false);
+        elapsedTime = 0;
+        gameManager.StartCountdownEnded();
+    }
+
+    private void GameManager_OnGameWon(object sender, EventArgs e)
+    {
+        gameWonPanel.SetActive(true);
+        gameWonMessage.text = "Congratulations! You Destroyed 20 cacti in " + elapsedTime.ToString("F2") + " seconds!";
+    }
+
+    private void GameManager_OnCactusDestroyed(object sender, GameManager.OnCactusDestroyedArgs e)
+    {
+        scoreText.text = "Score: " + e.score;
+    }
+
+    internal void UpdateAmmoUI(int currentAmmunition)
+    {
+        ammoText.text = "Ammo: " + currentAmmunition.ToString();
+    }
+}
